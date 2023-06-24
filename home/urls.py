7011501b7f0017778eb549fb6e032d4781e63446
Luiz Felipe Zomer <129988357+LuizZomer@users.noreply.github.com>
2023-06-23 21:45:02 -0300

    mais partes concluidas

--- home/urls.py
+++ home/urls.py
@@ -2,5 +2,6 @@ from django.urls import path, include
 from . import views
 
 urlpatterns = [
-    path('',views.home)
+    path('',views.home),
+    path('recipes/<int:id>/', views.recipe)
 ]
--- home/views.py
+++ home/views.py
@@ -1,5 +1,20 @@
 from django.shortcuts import render
+from util.recipes.factory import make_recipe
 
 # Create your views here.
 def home(request):
-    return render(request,'home/pages/home.html')
+
+    context = {
+        'recipes': [make_recipe() for _ in range(10)],
+    }
+
+    return render(request,'home/pages/home.html',context=context)
+
+
+def recipe(request,id):
+
+    context = {
+        'recipe': make_recipe
+    }
+
+    return render(request,'home/pages/recipe-view.html', context)
